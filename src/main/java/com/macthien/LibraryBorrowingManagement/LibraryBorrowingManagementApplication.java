b30fd74726e2fc01b2603006a4e8612d63c8da44
thienmacnguyen <macnguyenthien08082003@gmail.com>
2026-08-26 15:50:19 +0700

    Initial commit

---
+++ src/main/java/com/macthien/LibraryBorrowingManagement/LibraryBorrowingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.macthien.LibraryBorrowingManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBorrowingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBorrowingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macthien/LibraryBorrowingManagement/LibraryBorrowingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macthien.LibraryBorrowingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBorrowingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
